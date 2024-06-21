e6b1406f76e906fcd689218e3893b78836d12234
AnbumathiM <anbumathi1526@gmail.com>
2024-06-21 15:50:02 +0530

    commit

---
+++ ComcastCRMGUIFramework_Anbumathi/src/main/java/practicegit/SampleTest.java
@@ -0,0 +1,13 @@
+package practicegit;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void sample1() {
+		System.out.println("check");
+	}
+
+}
+
+
---
+++ ComcastCRMGUIFramework_Anbumathi/src/test/java/practice/TC_1Test.java
@@ -0,0 +1,12 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class TC_1Test {
+	@Test
+	
+public void practiceGitTest() {
+	 System.out.println("practice for git");
+	 System.out.println("TC_1 Updated");
+}
+}
